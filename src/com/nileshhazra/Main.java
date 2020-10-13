8aeee309bdd415cdddcf580edfaf8dcb3833ead2
nileshhazra <nileshhz99@gmail.com>
2020-10-14 01:40:43 +0530

    implement procedural design

--- src/com/nileshhazra/Main.java
+++ src/com/nileshhazra/Main.java
@@ -1,24 +1,77 @@
 package com.nileshhazra;
-import java.security.Principal;
 import java.text.NumberFormat;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
-        //Mortgage Calculator
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Principal: ");
-        int principal = scanner.nextInt();
-        System.out.print("Annual Interest Rate: ");
-        float Rate = scanner.nextFloat();
-        System.out.print("Period(Years): ");
-        int periodInYears = scanner.nextInt();
 
-        double monthlyInterestRate = (Rate/12)/100;
-        int numberOfPayments = periodInYears*12;
-        double mortgage = principal*((monthlyInterestRate*(Math.pow(1+monthlyInterestRate, numberOfPayments)))/(Math.pow(1+ monthlyInterestRate, numberOfPayments)-1));
-        String formattedMortgage = NumberFormat.getCurrencyInstance().format(mortgage);
-        System.out.println("Mortgage: " + formattedMortgage);
+        int principal = (int) readNumber("Principal ($1K - $1M): ", 1000, 1_000_000);
+        float annualInterest = (float) readNumber("Annual Interest Rate (1-30): ", 1, 30);
+        byte years = (byte) readNumber("Period(Years 1-30): ", 1, 30);
+
+        printMortgage(principal, annualInterest, years);
+        printPaymentSchedule(principal, annualInterest, years);
+
+
+    }
+
+    public static void printMortgage(int principal, float annualInterest, byte years) {
+        double mortgage = calculateMortgage(principal, annualInterest, years);
+        String formattedMortgage = NumberFormat.getCurrencyInstance(Locale.US).format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("---------");
+        System.out.println("Monthly payments: " + formattedMortgage);
+    }
+
+    public static void printPaymentSchedule(int principal, float annualInterest, byte years) {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("------------------");
+        for (short month = 1; month<= years * MONTHS_IN_YEAR; month++){
+            double balance = calculateBalance(principal, annualInterest, years, month);
+            System.out.println(NumberFormat.getCurrencyInstance(Locale.US).format(balance));
+        }
+    }
+
+
+    public static double readNumber(String prompt, double min, double max) {
+        Scanner scanner = new Scanner(System.in);
+        double value;
+        while (true) {
+            System.out.print(prompt);
+            value = scanner.nextDouble();
+            if(value>=min && value<=max)
+                break;
+            System.out.println("Accept value between " + min + " and " + max);
+        }
+        return value;
+    }
+
+
+    public static double calculateBalance(int principal, float annualInterest, byte years, short numberOfPaymentsMade) {
+
+
+        float monthlyInterest = (annualInterest / MONTHS_IN_YEAR) / PERCENT;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+
+        double balance = principal
+                *(Math.pow(1+ monthlyInterest, numberOfPayments) - Math.pow(1 + monthlyInterest, numberOfPaymentsMade))
+                / (Math.pow(1 + monthlyInterest, numberOfPayments) - 1);
+
+        return balance;
+    }
+
+    public static double calculateMortgage(int principal, float annualInterest, byte years) {
+
+        float monthlyInterest = (annualInterest / MONTHS_IN_YEAR) / PERCENT;
+        short numberOfPayments = (short) (years * MONTHS_IN_YEAR);
+        double mortgage = principal
+                *(monthlyInterest*(Math.pow(1+monthlyInterest, numberOfPayments)))
+                /(Math.pow(1+ monthlyInterest, numberOfPayments)-1);
+        return mortgage;
     }
 }
