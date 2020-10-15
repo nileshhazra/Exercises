eab89562d940e06074505b4ec85a59c1294efb86
nileshhazra <nileshhz99@gmail.com>
2020-10-15 11:14:17 +0530

    implement OOP design

---
+++ src/com/nileshhazra/MortgageReport.java
@@ -0,0 +1,33 @@
+package com.nileshhazra;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class MortgageReport {
+
+    private final NumberFormat currency;
+    private MortgageCalculator calculator;
+
+    public MortgageReport(MortgageCalculator calculator) {
+        this.calculator = calculator;
+        currency = NumberFormat.getCurrencyInstance(Locale.US);
+    }
+
+    public void printPaymentSchedule() {
+        System.out.println();
+        System.out.println("PAYMENT SCHEDULE");
+        System.out.println("------------------");
+        for (double balance : calculator.getRemainingBalances())
+            System.out.println(currency.format(balance));
+
+    }
+
+    public void printMortgage() {
+        double mortgage = calculator.calculateMortgage();
+        String formattedMortgage = currency.format(mortgage);
+        System.out.println();
+        System.out.println("MORTGAGE");
+        System.out.println("---------");
+        System.out.println("Monthly payments: " + formattedMortgage);
+    }
+}
